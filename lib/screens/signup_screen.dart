1728c01081aa2a8a998012c28c9a70cb59867bb1
Brett Morgan <brett.morgan@gmail.com>
2022-03-14 03:55:33 +0000

    Re-create runners, flutter format, dart fix

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,11 +1,11 @@
 import 'package:firebase_windows/Firebase/firebase_servcies.dart';
-import 'package:firebase_windows/screens/homeScreen.dart';
+import 'package:firebase_windows/screens/home_screen.dart';
 import 'package:firebase_windows/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
-import '../main.dart';
 ///Global Object for class firebase
-final firebaseServices=FirebaseService();
+final firebaseServices = FirebaseService();
+
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({Key? key}) : super(key: key);
 
@@ -14,8 +14,8 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
-  final emailController=TextEditingController();
-  final passwordController=TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
@@ -28,9 +28,13 @@ class _SignUpScreenState extends State<SignUpScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Center(child: Image.network("https://storage.googleapis.com/cms-storage-bucket/70760bf1e88b184bb1bc.png",height: 200,)),
+            Center(
+                child: Image.network(
+              "https://storage.googleapis.com/cms-storage-bucket/70760bf1e88b184bb1bc.png",
+              height: 200,
+            )),
             Form(
-              key:_formKey,
+              key: _formKey,
               child: Column(
                 children: [
                   Padding(
@@ -41,22 +45,24 @@ class _SignUpScreenState extends State<SignUpScreen> {
                           child: Padding(
                             padding: const EdgeInsets.all(8.0),
                             child: TextFormField(
-                              controller:emailController,
+                              controller: emailController,
                               validator: (value) {
-                                if(value!.isEmpty)
-                                  {
-                                    return "Email can't be blanked";
-                                  }
+                                if (value!.isEmpty) {
+                                  return "Email can't be blanked";
+                                }
+                                return null;
                               },
-                              decoration:  const InputDecoration(
+                              decoration: const InputDecoration(
                                 label: Text("Email"),
                                 contentPadding: EdgeInsets.all(10),
-                                floatingLabelBehavior: FloatingLabelBehavior.always,
-                                hintText:  "example.com",
+                                floatingLabelBehavior:
+                                    FloatingLabelBehavior.always,
+                                hintText: "example.com",
                                 focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.green,width: 1),
-                                  gapPadding: 10
-                                )
+                                  borderSide:
+                                      BorderSide(color: Colors.green, width: 1),
+                                  gapPadding: 10,
+                                ),
                               ),
                             ),
                           ),
@@ -73,25 +79,26 @@ class _SignUpScreenState extends State<SignUpScreen> {
                             child: Padding(
                               padding: const EdgeInsets.all(8.0),
                               child: TextFormField(
-                                controller:passwordController,
+                                controller: passwordController,
                                 validator: (value) {
-                                  if(value!.isEmpty)
-                                  {
+                                  if (value!.isEmpty) {
                                     return "Password can't be blanked";
-                                  }
-                                  else if(value.length<6) {
+                                  } else if (value.length < 6) {
                                     return "Password should be 6 characters long";
-
-                                  }},
-                                decoration:  const InputDecoration(
-                                    label: Text("Password"),
-                                    contentPadding: EdgeInsets.all(10),
-                                    floatingLabelBehavior: FloatingLabelBehavior.always,
-                                    hintText:  "*******",
-                                    focusedBorder: OutlineInputBorder(
-                                        borderSide: BorderSide(color: Colors.green,width: 1),
-                                        gapPadding: 10
-                                    )
+                                  }
+                                  return null;
+                                },
+                                decoration: const InputDecoration(
+                                  label: Text("Password"),
+                                  contentPadding: EdgeInsets.all(10),
+                                  floatingLabelBehavior:
+                                      FloatingLabelBehavior.always,
+                                  hintText: "*******",
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Colors.green, width: 1),
+                                    gapPadding: 10,
+                                  ),
                                 ),
                               ),
                             ),
@@ -106,11 +113,12 @@ class _SignUpScreenState extends State<SignUpScreen> {
             GestureDetector(
               onTap: () {
                 if (_formKey.currentState!.validate()) {
-                  firebaseServices.signUp(
-                      eml: emailController.text, ps: passwordController.text)
+                  firebaseServices
+                      .signUp(
+                          eml: emailController.text,
+                          ps: passwordController.text)
                       .then((user) {
-                    if(user!=null)
-                    {
+                    if (user != null) {
                       setState(() {
                         emailController.clear();
                         passwordController.clear();
@@ -118,21 +126,22 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       Navigator.pop(context);
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => const HomeScreen()),
+                        MaterialPageRoute(
+                            builder: (context) => const HomeScreen()),
                       );
                     }
-                  }).catchError((error){
+                  }).catchError((error) {
                     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                    elevation: 10,
-                    content: Text('Error!!!\n ${error.toString()}'),
-                    behavior: SnackBarBehavior.floating,
-                    shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(24),
-                    ),
-                    margin: EdgeInsets.only(
-                    bottom: MediaQuery.of(context).size.height - 100,
-                    right: 20,
-                    left: 20),
+                      elevation: 10,
+                      content: Text('Error!!!\n ${error.toString()}'),
+                      behavior: SnackBarBehavior.floating,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                      margin: EdgeInsets.only(
+                          bottom: MediaQuery.of(context).size.height - 100,
+                          right: 20,
+                          left: 20),
                     ));
                   });
                 }
@@ -142,41 +151,45 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 child: Container(
                   padding: const EdgeInsets.all(20),
                   alignment: Alignment.center,
-                  decoration:  BoxDecoration(
+                  decoration: BoxDecoration(
                     color: const Color(0XFF4D47C3),
                     borderRadius: BorderRadius.circular(16),
                   ),
-                  child: const Text("Sign up",style:  TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                  ),),
+                  child: const Text(
+                    "Sign up",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
                 ),
               ),
             ),
-            const Padding(padding: EdgeInsets.all(10),
-            child: Align(
-              alignment: Alignment.bottomRight,
-              child: Text(
-                "If you already have an account",
+            const Padding(
+              padding: EdgeInsets.all(10),
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  "If you already have an account",
+                ),
               ),
             ),
-            ),
-             Padding(padding:const EdgeInsets.only(left: 10,right: 10),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
               child: GestureDetector(
-                onTap: (){
+                onTap: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => const LoginScreen()),
+                    MaterialPageRoute(
+                        builder: (context) => const LoginScreen()),
                   );
                 },
                 child: const Align(
                   alignment: Alignment.bottomRight,
                   child: Text(
                     "Login Here!",
-                    style: TextStyle(
-                      color: Color(0XFF4D47C3)
-                    ),
+                    style: TextStyle(color: Color(0XFF4D47C3)),
                   ),
                 ),
               ),
